67a3599b3c669aadd36aed2a7fbab1520e349d62
Aviv Ronen <avivr00@gmail.com>
2025-08-22 14:53:29 +0300

    remove extra "/"

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -2,6 +2,6 @@
 // Contains the base URL for the backend API
 
 //const API_BASE_URL = "http://localhost:8000";   // for running the backend server locally
-const API_BASE_URL = "https://weatherstation-bm81.onrender.com/"  // Use a backend server from the internet
+const API_BASE_URL = "https://weatherstation-bm81.onrender.com";  // Use a backend server from the internet
 
 export { API_BASE_URL };
